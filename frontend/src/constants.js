76d2cdb753e6b330f5baca7bad6d0a100b7fb615
adodge-bcg <alan.dodge@gov.bc.ca>
2021-04-20 11:31:28 -0700

    Update constants to be consistent

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,8 @@
 const randomString = () => Math.random().toString(36).substring(2)
 
 export const INITIAL_IMPORT_USERS_TEXT = `\
-alice,Alice,Foo,alice@example.com,${randomString()},user-group|example-group
-bob,Bob,Bar,bob@example.com,${randomString()},admin-group|other-group
+alice,Alice,Foo,alice@example.com,${randomString()},user-group|admin-group
+bob,Bob,Bar,bob@example.com,${randomString()},admin-group|edit-group
 `
 
 export const INITIAL_IMPORT_GROUPS_TEXT = `user-group,admin-group,edit-group`
